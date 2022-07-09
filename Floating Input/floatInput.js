aa53b079ec8f764d56da112fca9d7c1ee8c1250a
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-10 02:25:56 +1200

    fixing bugs

--- Floating Input/floatInput.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.text-input').forEach(element => {
-    element.addEventListener('blur', event => {
-        if (event.target.value != '') {
-            event.target.nextElemnetSibling.classList.add('filled')
-        } else {
-            event.target.nextElemnetSibling.classList.remove('filled')
-        }
-    })
-})
