97aaff19db4ad71865b11d5d07ec698a463d7037
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-10 22:05:08 +1200

    arrows to move around boxes

--- one time password/otp.js
+++ one time password/otp.js
@@ -1,18 +1,10 @@
 const inputs = document.querySelectorAll('.otp-field input')
+
 inputs.forEach((input, index) => {
     input.dataset.index = index
-    input.addEventListener('past', handleOnPasteOtp)
+        // input.addEventListener('past', handleOnPasteOtp)
     input.addEventListener('keyup', handleOtp)
 })
-
-function handleOnPasteOtp(e) {
-    const data = e.clipboardData.getData('text')
-    const value = data.spit('')
-    if (value.length == inputs.length) {
-        inputs.forEach((input, index) => (input.value = value[index]))
-        submit()
-    }
-}
 
 function handleOtp(e) {
     const input = e.target
@@ -20,18 +12,27 @@ function handleOtp(e) {
     input.value = ''
     input.value = value ? value[0] : ''
 
-    let fieldIndex = input.dataset.index
-    if (value.length > 0 && fieldIndex < inputs.length - 1) {
+    if ((value.length > 0 || e.key == 'ArrowRight') && input.dataset.index < inputs.length - 1) {
         input.nextElementSibling.focus()
     }
 
-    if (e.key == 'Backspace' && fieldIndex > 0) {
+    if ((e.key == 'Backspace' || e.key == 'ArrowLeft') && input.dataset.index > 0) {
         input.previousElementSibling.focus()
     }
-    if (fieldIndex == inputs.lenght - 1) {
+    if (input.dataset.index == inputs.length - 1) {
         submit()
     }
 }
+
+
+// function handleOnPasteOtp(e) {
+//     const data = e.clipboardData.getData('text')
+//     const value = data.spit('')
+//     if (value.length == inputs.length) {
+//         inputs.forEach((input, index) => (input.value = value[index]))
+//         submit()
+//     }
+// }
 
 function submit() {
     let otp = ''
