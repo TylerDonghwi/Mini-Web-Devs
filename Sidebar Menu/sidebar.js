df5543504a85c2792f9a3abd51e3abaa486c831c
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-15 02:37:41 +1200

    text comes and goes

--- Sidebar Menu/sidebar.js
+++ Sidebar Menu/sidebar.js
@@ -1,31 +1,44 @@
 const items = document.querySelectorAll('.item')
+const menu = document.querySelector('.menu')
+const sideBar = document.querySelector('.side-bar')
+let sideBarOn = false
+const texts = document.querySelectorAll('.text')
 
 // initially home is selected
 items[0].style.left = '30px'
 
 items.forEach(item => {
     item.onclick = () => {
+        if (!sideBarOn) {
+            items.forEach(item => {
+                item.classList.remove('active')
+                item.style.left = '0'
+            })
+            item.classList.add('active')
+            item.style.left = '30px'
+        }
 
+    }
+})
+
+
+
+menu.addEventListener('click', () => {
+    if (sideBarOn) {
+        sideBar.style.width = '70px'
+        texts.forEach(text => {
+            text.style.visibility = 'hidden'
+            text.style.left = '-200px'
+        })
+    } else {
+        sideBar.style.width = '170px'
         items.forEach(item => {
             item.classList.remove('active')
             item.style.left = '0'
         })
-        item.classList.add('active')
-        item.style.left = '30px'
-    }
-})
-
-const menu = document.querySelector('.menu')
-const sideBar = document.querySelector('.side-bar')
-let sideBarOn = false
-menu.addEventListener('click', () => {
-    if (sideBarOn) {
-        sideBar.style.width = '70px'
-    } else {
-        sideBar.style.width = '220px'
-        items.forEach(item => {
-            item.classList.remove('active')
-            item.style.left = '0'
+        texts.forEach(text => {
+            text.style.visibility = 'visible'
+            text.style.left = '0px'
         })
     }
     sideBarOn = !sideBarOn
