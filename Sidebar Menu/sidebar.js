b779439c1c676997bc297b4612b7b24c073fa4b7
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-15 01:49:41 +1200

    smooth transition slide

--- Sidebar Menu/sidebar.js
+++ Sidebar Menu/sidebar.js
@@ -4,7 +4,11 @@ const items = document.querySelectorAll('.item')
 items.forEach(item => {
     item.onclick = () => {
 
-        items.forEach(item => item.classList.remove('active'))
+        items.forEach(item => {
+            item.classList.remove('active')
+            item.style.left = '0'
+        })
         item.classList.add('active')
+        item.style.left = '30px'
     }
 })
