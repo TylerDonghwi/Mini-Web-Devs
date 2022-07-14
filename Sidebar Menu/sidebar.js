68185c13fea52257034410ca924633a7b188cc63
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-15 02:18:02 +1200

    screen expands

--- Sidebar Menu/sidebar.js
+++ Sidebar Menu/sidebar.js
@@ -1,5 +1,7 @@
 const items = document.querySelectorAll('.item')
 
+// initially home is selected
+items[0].style.left = '30px'
 
 items.forEach(item => {
     item.onclick = () => {
@@ -11,4 +13,20 @@ items.forEach(item => {
         item.classList.add('active')
         item.style.left = '30px'
     }
+})
+
+const menu = document.querySelector('.menu')
+const sideBar = document.querySelector('.side-bar')
+let sideBarOn = false
+menu.addEventListener('click', () => {
+    if (sideBarOn) {
+        sideBar.style.width = '70px'
+    } else {
+        sideBar.style.width = '220px'
+        items.forEach(item => {
+            item.classList.remove('active')
+            item.style.left = '0'
+        })
+    }
+    sideBarOn = !sideBarOn
 })
